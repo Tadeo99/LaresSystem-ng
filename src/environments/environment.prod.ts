6064ee60c5a3422b894ccd7a6483e7651976659e
Tadeo99 <100897776+Tadeo99@users.noreply.github.com>
2024-08-02 00:11:16 -0500

    cang env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: false,
     //urlService: 'http://localhost:6500/',
     urlService: 'https://apiportallares.digitechgrouppe.com/',
-    urlAng : "http://localhost:4200/",
+    urlAng : "https://demoportaladmin.digitechgrouppe.com/",
     AngApiPath: 'http://localhost:4200/security',
     urlServiceSunat: 'https://api.apis.net.pe/',
-    angHome : "http://localhost:4200/",
+    angHome : "https://demoportaladmin.digitechgrouppe.com/",
     urlServiceDigitechso: 'https://apirestlares.digitechso.com/',
     urlSperant:'https://api.eterniasoft.com/v3/units/'
 };
