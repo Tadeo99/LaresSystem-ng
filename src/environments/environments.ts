f6861de4412947a138487be6090767bd5988edd6
Tadeo99 <100897776+Tadeo99@users.noreply.github.com>
2024-08-02 22:43:38 -0500

    ENV

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,7 @@ export const environment = {
     //urlService: 'http://localhost:6500/',    
     urlService: 'https://apiportallares.digitechgrouppe.com/',
     urlAng : "https://demoportaladmin.digitechgrouppe.com/",
-    AngApiPath: 'http://localhost:4200/security',
+    AngApiPath: 'https://demoportaladmin.digitechgrouppe.com/security',
     urlServiceSunat: 'https://api.apis.net.pe/',
     angHome : "https://demoportaladmin.digitechgrouppe.com/",
     urlServiceDigitechso: 'https://apirestlares.digitechso.com/',
